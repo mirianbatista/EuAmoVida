61bd957ce05091f6b0d3c059e360c5a7926f2624
Mírian <mirian.batista@dce.ufpb.br>
2018-09-18 14:18:00 -0300

    django funcionando

---
+++ lojinha/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from lojinha import views
+
+urlpatterns = [
+    path('', views.index, name='euamovida-lojinha'),
+]
---
+++ ondeestamos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OndeestamosConfig(AppConfig):
+    name = 'ondeestamos'
